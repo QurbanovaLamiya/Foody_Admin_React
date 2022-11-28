b51216a80ed6633f59a55e94e87f1e38120261ee
QurbanovaLamiya <lamiyaqurbanova121@gmail.com>
2022-11-28 17:16:24 +0400

    Product page nav section

--- src/features/Products/ProductsContainer.js
+++ src/features/Products/ProductsContainer.js
@@ -1,7 +1,25 @@
-const ProductsContainer = () =>{
-    return(
-        <></>
-    )
-}
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import ProductsContainerStyle from "./ProductsContainer.module.css";
 
-export default ProductsContainer
+const ProductsContainer = () => {
+  return (
+    <div className={ProductsContainerStyle.Container}>
+      <div className={ProductsContainerStyle.Caption}>
+        <h1>Products</h1>
+        <div className={ProductsContainerStyle.Select_Section}>
+          <select className={ProductsContainerStyle.Select}>
+            <option>Catagory type</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </select>
+          <KeyboardArrowDownIcon  className={ProductsContainerStyle.Icon}/>
+        </div>
+      </div>
+      <div className={ProductsContainerStyle.Content}>Container</div>
+      <div className={ProductsContainerStyle.Pagination}>Pagination</div>
+    </div>
+  );
+};
+
+export default ProductsContainer;
