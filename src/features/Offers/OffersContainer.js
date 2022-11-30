b1affffa0fba1db5bd1d47b731c4c3026f855b33
QurbanovaLamiya <lamiyaqurbanova121@gmail.com>
2022-11-30 16:30:23 +0400

    Created Offers page caption

--- src/features/Offers/OffersContainer.js
+++ src/features/Offers/OffersContainer.js
@@ -1,7 +1,19 @@
-const OffersContainer = () =>{
-    return(
-        <></>
-    )
-}
+import AddIcon from "@mui/icons-material/Add";
 
-export default OffersContainer
+import OffersStyle from "./Offers.module.css";
+
+const OffersContainer = () => {
+  return (
+    <div className={OffersStyle.Container}>
+      <div className={OffersStyle.Caption}>
+        <h1>Offers</h1>
+        <button>
+          <AddIcon />
+          ADD OFFER
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default OffersContainer;
